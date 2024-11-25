0fe0e4b3f666d84b9118c028206cfbe296ad4a03
rukhsarzia <rukhsar.zia@gmail.com>
2024-11-25 21:51:08 +0530

    "RENDER HTML PAGE"

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,8 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+# def home1(request):
+#     return HttpResponse("Hello")
 
 def home(request):
-    return HttpResponse("Hello")
+    posts = Post.objects.all
+    return render(request,'home.html',{"posts":posts})
